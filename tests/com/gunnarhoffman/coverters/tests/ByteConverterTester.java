fb706880ea42cc26466422baa3b593a053bc6147
bahair <blake.hair@gmail.com>
2013-04-22 13:26:54 -0400

    Added test case for adding kilobytes to bytes.

--- tests/com/gunnarhoffman/coverters/tests/ByteConverterTester.java
+++ tests/com/gunnarhoffman/coverters/tests/ByteConverterTester.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 import com.gunnarhoffman.converters.ByteConverter;
 
+import java.math.BigInteger;
+
 public final class ByteConverterTester {
 
 	@Test
@@ -13,4 +15,10 @@ public final class ByteConverterTester {
 		final long in = 1264978;
 		Assert.assertEquals(ByteConverter.fromBytes(in).toBytes(), in);
 	}
+
+    @Test
+    public void testAddKilobyteToOneThousandBytes() {
+        final BigInteger in = BigInteger.valueOf(2024);
+        Assert.assertEquals(ByteConverter.fromBytes(1000).addKilobytes(1).toBytes(), in);
+    }
 }
