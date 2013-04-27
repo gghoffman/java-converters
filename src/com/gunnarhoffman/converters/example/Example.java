36ed9c940fa430eb6dd090ff0b3af06795fd9aa9
Gunnar Hoffman <gunnar.g.hoffman@gmail.com>
2013-04-27 17:15:22 -0400

    added another blank test class

--- src/com/gunnarhoffman/converters/example/Example.java
+++ src/com/gunnarhoffman/converters/example/Example.java
@@ -1,6 +1,7 @@
 package com.gunnarhoffman.converters.example;
 
 import com.gunnarhoffman.converters.ByteConverter;
+import com.gunnarhoffman.converters.DistanceConverter;
 import com.gunnarhoffman.converters.TimeConverter;
 
 public class Example {
@@ -8,10 +9,10 @@ public class Example {
 	public static void main(String[] args) {
 
 		System.out.println(ByteConverter.fromMegabytes(6000)
-										.addGigabytes(-1.34)
-										.toKilobytes()
-										.floatValue());
+				.addGigabytes(-1.34));
 
 		System.out.println(TimeConverter.fromWeeks(467));
+
+		System.out.println(DistanceConverter.fromKilometers(460000000));
 	}
 }
